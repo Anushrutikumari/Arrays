4a9a8edc2b9d142488565ae0bef21a12e4dff41c
Anushruti kumari <anushrutikumari2@gmail.com>
2023-01-22 23:37:08 +0530

    loops

---
+++ LOOPS/FUNCTIONS/1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int getmax(int x,int y);
+int main()
+{
+  int a=10,b=20;
+  cout<<get(a,b);
+  return 0;
+
+}
---
+++ LOOPS/FUNCTIONS/2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+  double a=10,b=20;
+  cout<<pow(a,b)<<endl;
+  double x=100;
+
+  cout<<log10(x);
+  return 0;
+
+}
